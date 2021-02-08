cba1672845005dffb78b011b1f1d0bbc000ec20f
Jordan Perucho <jpo88fv1@gmail.com>
2021-02-08 23:51:17 +0100

    jordan -> marked todo's

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,7 +1,7 @@
 docker volume prune
 docker container prune
 
-if [ ${0} == "-a" ] || [ ${0} == "--all" ]; then
+if [ "$1" = "-a" ] | [ "$1" = "--all" ]; then
     containers="$(docker container list | cut -d" " -f1)"
     for container in $containers
     do
